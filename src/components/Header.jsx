437b0b78144ed61162640c09bf2ac8faa8187ce2
bikashroy120 <bikashroydt@gmail.com>
2023-03-09 15:48:18 +0600

    all add

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className="header-holder navbar-top">
+        <div>
+            {/* <img src={logo} alt="" /> */}
+            <h2>logo here</h2>
+        </div>
+        <div className="navbar-collapse">
+            <ul>
+              <li>Lin</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div className='home_wappper'>
+        <div className="hero-box">
+            <div className="custome-container">
+                <Header/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
