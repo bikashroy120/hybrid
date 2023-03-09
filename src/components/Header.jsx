b5fef7c5df301178578a5db75bf01ea97976fcd9
bikashroy120 <bikashroydt@gmail.com>
2023-03-09 17:28:17 +0600

    addd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 
 const Header = () => {
+
+  const location = useLocation()
+  const [link,setLink] = useState(location?.pathname)
+
   return (
-    <div className="header-holder navbar-top">
+    <div className="navbar-top">
         <div>
             {/* <img src={logo} alt="" /> */}
             <h2>logo here</h2>
         </div>
         <div className="navbar-collapse">
-            <ul>
-              <li>Lin</li>
+            <ul className='nav-list'>
+              <li><Link className={link === "/" ? "nav-item active" : "nav-item"} to={"/"}>Home</Link> </li>
+              <li><Link className={link === "/techer" ? "nav-item active" : "nav-item"} to={"/techer"}>Techear</Link> </li>
+              <li><Link className={link === "/about" ? "nav-item active" : "nav-item"} to={"/about"}>About</Link> </li>
+              <li><Link className={link === "/blog" ? "nav-item active" : "nav-item"} to={"/blog"}>Blog</Link> </li>
+              <li><Link className={link === "/contact" ? "nav-item active" : "nav-item"} to={"/contact"}>Contact</Link> </li>
             </ul>
         </div>
+
+
+        
     </div>
   )
 }
